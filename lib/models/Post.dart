2baa15af19b8237b1b1c0d4993e3ace8d9d6aed7
조연희 <yhcho.proj@gmail.com>
2025-12-26 12:19:39 +0900

    scroll

--- lib/models/Post.dart
+++ lib/models/Post.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
+// =======================================================================
 // 아래와 같이 클래스
 // Post({ required this.postId,
 // factory Post.fromJson(Map<String, dynamic> json) {
@@ -25,7 +26,8 @@ import 'dart:convert';
 // (window) Alt + insert (mac) Cmd + N
 // Generator -> fromJson/toJson 선택하면 자동 완성된다.
 
-
+// Alt + insert -> 생성자 선택
+// =======================================================================
 class Post {
   int postId;
   int userId;
@@ -39,11 +41,6 @@ class Post {
   int likeCount;
   int commentCount;
 
-  // 기본 생성자를 만드는 작업을 아래와 같이 작성
-  // Post(int postId, String postName) {
-  //    this.postId = postId
-  //    this.postName = postName
-  // }
   Post({
     required this.postId,
     required this.userId,
@@ -58,13 +55,8 @@ class Post {
     required this.commentCount,
   });
 
-  // Json 형식 = web에서 사용하는 형식
-  // 어플에서는 Json 형식을 사용하려면 매번 json['postId'] 와 같이
-  // web 에서 가져 온 json 형식에서 'postId'라는 명칭으로 지칭 된 데이터를 사용하겠다.
-  // 모든 변수 이름 앞에 json 을 붙이고 [] 로 표현
-  // 직접 길게 작성하고 싶지 않기 때문에
-  // 백엔드에서 앱으로 가져 온 json 형태를 postId 라는 변수 명칭에 대입하여
-  // json[] 형식이 없어도 데이터를 표현 가능하도록 설정
+  // fromJson 은 백엔드에서 가져 온 데이터를 dart 보여주는 형식
+  // toJson 은 어플에서 백엔드로 데이터를 저장할 때 사용하는 형식
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
       postId: json['postId'],
@@ -80,4 +72,29 @@ class Post {
       commentCount: json['commentCount'],
     );
   }
+  
+  // Dart Data Class Generator-> 인텔리제이 자동으로 사용하는 플러그인 설정
+
+  // from dart to dart 만드는 형식
+
+  // 라이브 템플릿에서 변수 편집까지 끝내면 내부 코드 또한 자동으로 완성되는 템플릿 완성 가능
+
+  // Live Template 만들기 형식을 통해
+  // 우리가 직접  alt + insert 에서 생성자나 getter / setter 를 클릭하면 만들어주는
+  // 기능을 설정
 }
+
+// 기본 생성자를 만드는 작업을 아래와 같이 작성
+// Post(int postId, String postName) {
+//    this.postId = postId
+//    this.postName = postName
+// }
+
+
+// Json 형식 = web에서 사용하는 형식
+// 어플에서는 Json 형식을 사용하려면 매번 json['postId'] 와 같이
+// web 에서 가져 온 json 형식에서 'postId'라는 명칭으로 지칭 된 데이터를 사용하겠다.
+// 모든 변수 이름 앞에 json 을 붙이고 [] 로 표현
+// 직접 길게 작성하고 싶지 않기 때문에
+// 백엔드에서 앱으로 가져 온 json 형태를 postId 라는 변수 명칭에 대입하여
+// json[] 형식이 없어도 데이터를 표현 가능하도록 설정
---
+++ lib/services/PostService.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter_project/models/Post.dart';
+import 'package:http/http.dart' as http;
+
+class Postservice {
+  static String baseUrl = 'http://localhost:9000';
+
+  // 이미지 URL 만들기
+  static String getImageUrl(String path) {
+    return baseUrl + path;
+  }
+
+  // 어플에서는 백엔드에서 데이터를 가져온 다는 것이 기본 세팅은 아님
+  // 백엔드 연결 없이 핸드폰 내부에 localstorage 처럼 데이터 저장 공간을 만들어서
+  // 소비자가 데이터를 관리하고,
+  // 핸드폰에서 제공하는 게임이나 todoList 와 같은 작업을 하는 어플이 기본이기 때문
+  // 게시물 백엔드에서 어플로 가져오기
+  // springboot 나 dart 는 dependencies 에 htpp web 연결 관련된 모듈 설치해야지 가능
+  // http 의 import 가 보이지 않을 때는
+  // pubspec.yaml 로 접속하여 dependencies 에 http 관련 모듈 세팅이 존재하는지 확인
+  static Future<List<Post>> getPosts() async{
+    var res = await http.get(Uri.parse('$baseUrl/api/posts'));
+
+    if(res.statusCode == 200) {
+      // 데이터를 가지고 올 때 영어 숫자 이 외 글자 깨짐 없도록 세팅
+      List jsonList = json.decode(utf8.decode(res.bodyBytes));
+      return jsonList.map((j) => Post.fromJson(j)).toList();
+    } else {
+      throw Exception('데이터를 불러올 수 없습니다.');
+    }
+  }
+}
